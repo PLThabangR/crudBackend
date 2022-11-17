210973c31edb6c8fe725dd1d6b139ab7ddf45a12
ThabangR <rakgoropothabang@gmail.comgit config --global user.email>
2022-11-17 15:02:52 +0200

    Created a controller for post and successfully connected the spring to the Mysql database

--- src/main/java/com/Backend/FullStackBackend/controller/UserController.java
+++ src/main/java/com/Backend/FullStackBackend/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.Backend.FullStackBackend.controller;
 
 
+import com.Backend.FullStackBackend.exception.UserNotFoundException;
 import com.Backend.FullStackBackend.model.User;
 import com.Backend.FullStackBackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -17,5 +19,37 @@ public class UserController {
     User newUser(@RequestBody User newUser){
 
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List <User> getAllUsers(){
+
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+
+        return "User with id "+id+" has been deleted";
     }
 }
---
+++ src/main/java/com/Backend/FullStackBackend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Backend.FullStackBackend.exception;
+
+public class UserNotFoundException  extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("User not found ");
+    }
+}
